d633811fe9ba5cd9b1daf0fd634d0bf75659cd4b
lethmate <lethmate@pen2.de>
2015-12-01 14:31:21 +0100

    missing files

--- app/src/main/java/mdb/de/rating/LocationsFragment.java
+++ app/src/main/java/mdb/de/rating/LocationsFragment.java
@@ -2,8 +2,10 @@ package mdb.de.rating;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,9 +13,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 /**
- * Created by LethmateB on 09.11.2015.
+ * Created by LethmateB on 19.11.2015.
  */
-public class ListActivity extends AppCompatActivity{
+public class LocationsFragment extends Fragment {
+
     ArrayList<Spot> spots;
     ListView spotList;
     SpotAdapter arrayAdapter;
@@ -21,14 +24,20 @@ public class ListActivity extends AppCompatActivity{
     TextView city;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActivity().setContentView(R.layout.activity_main); // Inflate the layout for this fragment
 
         this.currentPosition = new CurrentPosition();
-        this.city = (TextView) findViewById(R.id.city);
+        this.city = (TextView) getActivity().findViewById(R.id.city);
 
-        this.city.setText(this.currentPosition.getCity());
+        //TODO: locate me
+        if(this.currentPosition.getCity() != null) {
+            this.city.setText(this.currentPosition.getCity());
+        }
+
         showSpotsInCity();
+        return inflater.inflate(R.layout.location_view, container, false);
     }
 
     /**
@@ -36,15 +45,15 @@ public class ListActivity extends AppCompatActivity{
      */
     private void showSpotsInCity() {
         String city = this.currentPosition.getCity();
-        this.spotList = (ListView) findViewById(R.id.listView);
-        RateemDatabaseHelper helper = new RateemDatabaseHelper(this);
+        this.spotList = (ListView) getActivity().findViewById(R.id.listView);
+        RateemDatabaseHelper helper = new RateemDatabaseHelper(getActivity());
 
         this.spots = helper.getSpotsForCity(city, 0);
         for (Spot spot: this.spots) {
             spot.setDistance(calculateDistance(spot));
         }
         this.arrayAdapter = new SpotAdapter(
-                this,
+                getActivity(),
                 R.layout.list_items,
                 this.spots);
 
@@ -53,21 +62,14 @@ public class ListActivity extends AppCompatActivity{
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Spot spot = (Spot) spotList.getItemAtPosition(position);
-                Intent intent = new Intent(ListActivity.this, RatingActivity.class);
+                Intent intent = new Intent(getActivity(), RatingActivity.class);
                 intent.putExtra("id", spot.getId());
                 startActivity(intent);
             }
         });
     }
 
-    /**
-     *
-     * Calculates the distance from the last current position to a spot.
-     * @param spot the spot
-     * @return the distance from my location to the spot
-     */
     public float calculateDistance(Spot spot) {
-        CurrentPosition position = new CurrentPosition();
-        return spot.location.distanceTo(position.getLocation());
+        return (float) 0.0;
     }
 }
--- app/src/main/java/mdb/de/rating/MainActivity.java
+++ app/src/main/java/mdb/de/rating/MainActivity.java
@@ -1,65 +1,112 @@
 package mdb.de.rating;
 
-import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
-/**
- * The main activity. The app starts here.
- * Insert the app loading screen and the intro.
- * The activity calls the fragments in the end.
- */
-public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Called when the activity is created. Sets up some basic settings.
-     * @param savedInstanceState the state of the instance
-     */
+/**
+ * Created by LethmateB on 09.11.2015.
+ */
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    private boolean viewIsAtHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Intent intent = new Intent(MainActivity.this, ListActivity.class);
-        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        displayView(R.id.nav_locations);
+    }
+
+
+    /**
+     *
+     * Calculates the distance from the last current position to a spot.
+     * @param spot the spot
+     * @return the distance from my location to the spot
+     */
+    public float calculateDistance(Spot spot) {
+        CurrentPosition position = new CurrentPosition();
+        return spot.location.distanceTo(position.getLocation());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        if (!viewIsAtHome) { //if the current view is not the News fragment
+            displayView(R.id.nav_locations); //display the News fragment
+        } else {
+            moveTaskToBack(true);  //If view is in News fragment, exit application
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        displayView(item.getItemId());
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void displayView(int viewId) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onClick(View view) {
-        Intent intent = null;
-        switch (view.getId()) {
-
-            default:
+        switch (viewId) {
+            case R.id.nav_locations:
+                fragment = new LocationsFragment();
+                title  = "Orte";
+                this.viewIsAtHome = true;
                 break;
+            case R.id.nav_near_to_me:
+                //TODO: finish
+                break;
+            case R.id.nav_search:
+                fragment = new SearchFragment();
+                title  = "Suche";
+                this.viewIsAtHome = false;
+                break;
+
         }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
     }
 }
